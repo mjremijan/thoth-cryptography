eb46561bd8b47db88f17e4e6f23d7837272fcb13
Michael Remijan <mjremijan@yahoo.com>
2017-11-07 07:47:24 -0600

    Finishing SHA512 hash research.  Ready for article.

--- thoth-security-02-hash-sha512/README.md
+++ thoth-security-02-hash-sha512/README.md
@@ -4,4 +4,14 @@
 
 How to do a SHA-512 hash with a salt.
 
-Research shows the most common way is to concatenate `toBeHashed + salt` and then hash.
+Research shows the most common way generate a hash with a value and a salt is to first concatenate `toBeHashed + salt` and then hash.
+
+Research was conducted on the following sites to verify the Java code is generating a correct hash:
+
+* http://www.convertstring.com/Hash/SHA512
+* http://www.lorem-ipsum.co.uk/hasher.php
+* http://passwordsgenerator.net/sha512-hash-generator/ 
+    - NOTE: with this website there is no 'salt' option.  So instead I concatinated `toBeHashed + salt`
+* https://hash.online-convert.com/sha512-generator
+    - NOTE: with this website there is no 'salt' option.  So instead I concatinated `toBeHashed + salt`
+     
---
+++ thoth-security-02-hash-sha512/src/test/java/org/thoth/security/hash/sha512/SHA512Test.java
@@ -0,0 +1,46 @@
+package org.thoth.security.hash.sha512;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ *
+ * @author Michael Remijan mjremijan@yahoo.com @mjremijan
+ */
+public class SHA512Test {
+
+    @Test
+    public void test_hash_to_hex() throws Exception {
+        // setup
+        String username = "mjremijan";
+        String password = "super!secret";
+        SHA512 sha = new SHA512();
+
+        // test
+        String asHex
+            = sha.hashToHex(password, username);
+
+        // assert
+        Assert.assertEquals(
+              "F38CD5290D11B20159E36740843A8D93CFDFA395CF594F328613EF5C7BA42D9EAC00BF3EE47B7E8CE1587040B36365F05C8E15E9392C288A1D7C4CFB66097848"
+            , asHex);
+    }
+
+
+    @Test
+    public void test_hash_to_base64() throws Exception {
+        // setup
+        String username = "mjremijan";
+        String password = "super!secret";
+        SHA512 sha = new SHA512();
+
+        // test
+        String asBase64
+            = sha.hashToBase64(password, username);
+
+        // assert
+        Assert.assertEquals(
+              "84ZVKQ0RSGFZ42DAHDQNK8/FO5XPWU8YHHPVXHUKLZ6SAL8+5HT+JOFYCECZY2XWXI4V6TKSKIODFEZ7ZGL4SA=="
+            , asBase64);
+    }
+}
